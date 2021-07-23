382a8e756da6a807bd035ebeff3123e1f21ea677
MrWalshyType2 <mwalsh@qa.com>
2021-07-23 14:34:16 +0100

    Updated example for using fetch statements

--- examples/using-fetch/js/app.js
+++ examples/using-fetch/js/app.js
@@ -6,40 +6,70 @@
     let title = document.querySelector("#title");
     let body = document.querySelector("#body");
     let submitBtn = document.querySelector("#submitBtn");
+    let userId = document.querySelector("#userId");
 
     // methods and functions
-    let initiateFetch = (options) => {
+    function Post(title, body, userId) {
+        this.title = title;
+        this.body = body;
+        this.userId = userId;
+    }
+
+    let fetchWithoutBody = (options) => {
         if (options.id) options.url = `${options.url}/${options.id}`;
 
         fetch(options.url, {
-            method: options.method,
-            headers: 
-                options.method === "POST" || options.method === "PUT"
-                ? {
-                    'Content-type': 'application/json; charset=UTF-8'
-                }
-                : {},
-            body:
-                options.title !== null || options.title !== ""
-                ? JSON.stringify(options)
-                : {}
+            method: options.method
         }).then(response => {
             return response.json();
         }).then(data => {
             console.log(data);
+            // append a table to the dom with the returned data
+        }).catch(e => console.error(e));
+    }
+
+    let fetchWithBody = (options) => {
+        if (options.id) options.url = `${options.url}/${options.id}`;
+
+        fetch(options.url, {
+            method: options.method,
+            headers:{
+                    'Content-type': 'application/json; charset=UTF-8'
+            },
+            body:
+                JSON.stringify(new Post(options.title, options.body, options.userId))
+        }).then(response => {
+            return response.json();
+        }).then(data => {
+            console.log(data);
+            // append data to the dom
         }).catch(e => console.error(e));
     }
 
     // listeners and inits
     submitBtn.addEventListener("click", (event) => {
         // prevent default form submission behaviour
-        event.preventDefault();
-        initiateFetch({
+        // event.preventDefault(); // or set the buttons type to button in this case
+        console.log(event.target);
+
+        // modify style
+        // event.target.style = "background-color: blue; display: none;";
+        // modify classes
+        event.target.className = "some-class";
+
+        let options = {
             method: httpMethod.value,
             url: url.value,
             id: id.value,
             title: title.value,
             body: body.value,
-        });
+            userId: userId.value
+        };
+
+        if (options.method === "GET" || options.method === "HEAD") {
+            fetchWithoutBody(options);
+        } else {
+            fetchWithBody(options);
+        }
     }, false);
 })();
